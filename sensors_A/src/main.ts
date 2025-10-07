2421c401ca62ef40bcc89d0a451d1bb2601bdf2f
mobiwek61 <mobiwek61@gmail.com>
2025-10-07 13:16:19 +0000

    sensors

--- sensors_A/src/main.ts
+++ sensors_A/src/main.ts
@@ -1,13 +1,32 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import './style.css'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+//import { setupCounter } from './counter.ts'
 import { sensors } from './util/sensorHelper.ts';
+import { QuatAreFun } from './util/QuatAreFun.ts'
 
-document.querySelector<HTMLDivElement>('#divA')!.innerHTML = `
-  <div>
-    this is divA ${sensors.xyz()}
-  </div>
-`
+document.querySelector<HTMLDivElement>('#divA')!.innerHTML = 'this is divA'; 
+setDisp('hello2')
+setDisp(sensors.xyz())
+function setDisp(str:string) {
+  //console.log(str)
+  document.querySelector<HTMLDivElement>('#divA')!.innerHTML = 'ff ' + str
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+var sensorOrien:any
+// //var currQuat = useRef(new Quaternion())
+function setupOrien() {
+        //kalmanFilter_ref.current = KalmanFilter.okDamped()//.slowDamped()
+        try {
+          // ct.current++ // make sure get here only once
+          // addEventListener inside useEffect() OR ELSE ADDS hundreds of times!!
+          sensorOrien = new AbsoluteOrientationSensor({ frequency: 22, referenceFrame: "device" });
+          sensorOrien.addEventListener("reading", () => {
+              var eulAngles:any = QuatAreFun.quatEuler(sensorOrien.quaternion)
+              //currQuat.current = sensorOrien.quaternion
+              //var filtElev = Math.round(kalmanFilter_ref.current!.update(eulAngles.pitch))
+          });
+          sensorOrien.start();
+        } catch (ex) {} 
+      }
+//setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
