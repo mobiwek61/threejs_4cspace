b4191cb1d17400fa2c4f83ccba03447c2b21630e
mobiwek61 <mobiwek61@gmail.com>
2025-10-17 16:10:41 +0000

    intermed

--- threeDemos/src/menuNdispatcherFnCallback.tsx
+++ threeDemos/src/menuNdispatcherFnCallback.tsx
@@ -4,10 +4,10 @@ import { doAllWork as demoB } from './demos/demoB.ts';
 import { TwoSpheresWithMovingLight } from './demos/OrbitLights/DemoA.ts'
 // import { PopupA, QrInFrame } from './util/PopupA.ts';
 import type { MenuItemLink, MenuItemFnCall } from './util/HamburgerMenu.ts';
-import { Hamburger } from './util/HamburgerMenu.ts';
+import { CreateHamburgerMenuLinks } from './util/HamburgerMenu.ts';
 
 
-var demoID = new URLSearchParams(window.location.search).get('demoID')
+
 const menuItems:Array<MenuItemLink> = [
   //{ href: "?demoID=demoID-A", text: "texture circles" },
   { href: "?demoID=demoID-B&anisotropic=16&textureSpec=circle", text: "texture circle aniso-16" },
@@ -15,9 +15,17 @@ const menuItems:Array<MenuItemLink> = [
   { href: "?demoID=demoID-B&anisotropic=16", text: "texture stars aniso-16" },
   { href: "?demoID=OrbitLights", text: "OrbitLights" },
 ];
+const menuItemszzzz:Array<MenuItemFnCall> = [
+  //{ href: "?demoID=demoID-A", text: "texture circles" },
+  { cback: demoB, text: "texture circle aniso-16" },
+  { cback: demoB, text: "texture stars aniso-0" },
+  { cback: demoB, text: "texture stars aniso-16" },
+  { cback: demoB, text: "OrbitLights" },
+];
 
 let frameElem: HTMLElement | null = document.querySelector("#mainDiv");
 
+var demoID = new URLSearchParams(window.location.search).get('demoID')
 switch (demoID) {
     //case 'demoID-A': demoA(); break;
     case 'demoID-B': 
@@ -31,4 +39,4 @@ switch (demoID) {
 }
 
 document.body.appendChild(
-    Hamburger((ff:void)=>{ console.log('hi2')} , menuItems)   )
+    CreateHamburgerMenuLinks((ff:void)=>{ console.log('hi2')} , menuItems)   )
--- threeDemos/src/util/HamburgerMenu.ts
+++ threeDemos/src/util/HamburgerMenu.ts
@@ -2,8 +2,12 @@
 import './Hamburger.css';
 import { QRpopup } from './QRpopup';
 
+type MenuItemLink = { href: string;   text: string; };
+// type callbackA = () => void;
+type MenuItemFnCall = { cback: () => void; text: string; };
+
 var theQR = QRpopup({})
-function Hamburger(ff: () => void, menus:Array) {
+function CreateHamburgerMenuLinks(ff: () => void, menus:Array) {
   console.log('asdf')
   const container = document.createElement('div');
   container.className = 'hamburger';
@@ -19,12 +23,28 @@ function Hamburger(ff: () => void, menus:Array) {
     bar.className = 'bar';
     container.appendChild(bar);
   }
-  container.appendChild(parseMenu(menus));
+  container.appendChild(parseMenuToLink(menus));
   container.appendChild(theQR) // the {} removes missing-arg error
   return container;
 }
 
-const parseMenu = (links:Array<Object>) => {
+const parseMenuToFn = (links:Array<MenuItemFnCall>) => {
+  const div = document.createElement('div'); div.id = 'menuFrame'; div.style.display = 'none'
+  const menuDiv = document.createElement('div'); div.id = 'menuA'; 
+  menuDiv.style.color = 'white'; menuDiv.textContent = 'choose item: '; 
+  /* stack vertically */menuDiv.style.display='flex'; menuDiv.style.flexDirection='column';
+  menuDiv.style.whiteSpace = 'nowrap'; 
+  links.forEach(link => {
+    const anch = document.createElement('a'); anch.className='anchorStyle'; 
+    anch.href = link.href;  anch.textContent = link.text; 
+    menuDiv.appendChild(anch);
+  });
+  createButton('qr code', () => { console.log('adfasdf')}, menuDiv)
+  div.appendChild(menuDiv)
+  return div
+};
+
+const parseMenuToLink = (links:Array<MenuItemLink>) => {
   const div = document.createElement('div'); div.id = 'menuFrame'; div.style.display = 'none'
   const menuDiv = document.createElement('div'); div.id = 'menuA'; 
   menuDiv.style.color = 'white'; menuDiv.textContent = 'choose item: '; 
@@ -49,6 +69,8 @@ function createButton(label: string, onClick: () => void, target: HTMLElement = 
     
   target.appendChild(button);
 }
+
+export { CreateHamburgerMenuLinks, type MenuItemLink, type MenuItemFnCall };
 
 // Example usage:
 //document.body.appendChild(Hamburger());
@@ -78,4 +100,4 @@ function createButton(label: string, onClick: () => void, target: HTMLElement = 
   // );
 // };
 
-export { Hamburger };
+
